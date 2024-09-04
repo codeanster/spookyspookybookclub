fd7f98588b5cab11054de0210bfe9dac3855049b
Jake Tucker <jake.tucker@live.com>
2024-09-04 12:00:47 -0700

    Fixed an issue where query had no value and when searching to the query entered by user it would user the empty value instead of entered value.

--- lambda/google_books_api.py
+++ lambda/google_books_api.py
@@ -9,15 +9,14 @@ api_key = os.getenv('API_KEY')
 
 #create base url to make request call to google books API
 
-query = None
+query = input('Book?: ')
 base_url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}'
-
-input = input('Book?: ')
 
 response = requests.get(base_url)
 
 if response.status_code== 200:
     print('success')
-    
+    print(response.json())
+
 else:
     print('fail')
