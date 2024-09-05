d94299e8312d79faef189c70216ba41f88b5673a
unknown <gonco1@live.com>
2024-09-04 21:22:11 -0700

    converted to AWS lambda

--- lambda/test_curls.bash
+++ lambda/test_curls.bash
@@ -1,1 +1,5 @@
 curl -X GET "https://k67t787b4l.execute-api.us-west-1.amazonaws.com/Prod/getUpcomingEvents"
+
+curl -X GET "https://k67t787b4l.execute-api.us-west-1.amazonaws.com/Prod/getBooks?title=Ghost_Eaters"
+
+curl -X GET "https://k67t787b4l.execute-api.us-west-1.amazonaws.com/Prod/getBook?title=Ghost_Eaters"
